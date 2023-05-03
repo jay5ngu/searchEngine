5897a1e4828a585943baef33fd8018b49483b4ee
Ethan Wong <wongea2@uci.edu>
2023-05-03 02:19:12 -0700

    updated robots permissions check - leave code in worker.py since downloading URL needs access to logger / config objects - cache RobotFileParser objects to minimize web requests sent to cache server & improve crawler speed - only search cache server for robots.txt, avoid bypassing it

--- crawler/worker.py
+++ crawler/worker.py
@@ -1,14 +1,12 @@
-import http.client
-import urllib.error
 from threading import Thread
 from urllib import robotparser
-from urllib.parse import urlparse
+from urllib.parse import urlparse, ParseResult
 from inspect import getsource
 from utils.download import download
 from utils import get_logger
+from typing import Dict
 import scraper
 import time
-
 
 class Worker(Thread):
     def __init__(self, worker_id, config, frontier):
@@ -20,37 +18,61 @@ class Worker(Thread):
         assert {getsource(scraper).find(req) for req in {"from urllib.request import", "import urllib.request"}} == {-1}, "Do not use urllib.request in scraper.py"
         super().__init__(daemon=True)
 
+    def is_not_prohibited(self, url, prohibited_urls: Dict[str, robotparser.RobotFileParser]):
+        # extract url components
+        parsed: ParseResult = urlparse(url)
+        netloc = scraper.ReportStatisticsShelf.normalize_url(parsed.netloc)  # for consistency
+
+        if netloc in prohibited_urls:
+            # we already checked for this website's robots.txt rules
+            if prohibited_urls[netloc]:
+                # we successfully found rules
+                return prohibited_urls[netloc].can_fetch('*', url)
+            else:
+                # website doesn't have or has an invalid robots.txt -- crawl anyways
+                return True
+        else:
+            # we need to check for this website's robots.txt rules
+
+            # generate robots.txt URL
+            robots_url: str = parsed._replace(path='/robots.txt', fragment='').geturl()
+
+            # attempt to retrieve robots.txt (from cache server) - don't bypass cache
+            try:
+                # download from cache
+                resp = download(robots_url, self.config, self.logger)
+                self.logger.info(
+                    f"Robots file downloaded {robots_url}, status <{resp.status}>, "
+                    f"using cache {self.config.cache_server}.")
+                # check response content
+                if not resp or resp.status != 200 or not resp.raw_response or not resp.raw_response.content:
+                    # no robots.txt found or it is empty
+                    prohibited_urls[netloc] = None
+                    return True
+
+                # need to create new parser
+                rp: robotparser.RobotFileParser = robotparser.RobotFileParser()
+                # parse the robots.txt content
+                rp.parse(resp.raw_response.content)
+                # save for future use
+                prohibited_urls[netloc] = rp
+                # verify site crawl-ability
+                return rp.can_fetch('*', url)
+            except Exception as e:
+                self.logger.info(f"Robots.txt parsing error on {robots_url}.")
+                prohibited_urls[netloc] = None  # invalid robots.txt
+                return True  # by default, enable crawling
 
     def run(self):
-        no_robot_file_found = False
-        restricted_urls = set()
+        prohibited_urls: Dict[str, robotparser.RobotFileParser] = {}  # keeping local since no multithreading
         while True:
             tbd_url = self.frontier.get_tbd_url()
             if not tbd_url:
                 self.logger.info("Frontier is empty. Stopping Crawler.")
                 break
 
-            # robots check
-            url_components = urlparse(tbd_url)
-            # check if site contains robots.txt - only lives in root directory (sites in frontier are already defrag)
-            # if not url_components.path and not url_components.query:
-            # is a root dir URL
-            robots_parser = robotparser.RobotFileParser(str(url_components.scheme).rstrip("/") + "://" + str(url_components.netloc).rstrip("/") + "/robots.txt")
-            try:
-                robots_parser.read()
-                # TODO : add restricted URLs to our master set
-            except urllib.error.URLError:
-                no_robot_file_found = True
-            except http.client.InvalidURL:
-                no_robot_file_found = True
-            except UnicodeDecodeError:
-                no_robot_file_found = True
-            except UnicodeEncodeError:
-                no_robot_file_found = True
-            except Exception:
-                no_robot_file_found = True
-
-            if no_robot_file_found or robots_parser.can_fetch("*", str(tbd_url)):
+            # check site's robots.txt permissions before downloading URL content
+            if self.is_not_prohibited(tbd_url, prohibited_urls):
                 resp = download(tbd_url, self.config, self.logger)
                 self.logger.info(
                     f"Downloaded {tbd_url}, status <{resp.status}>, "
@@ -60,9 +82,7 @@ class Worker(Thread):
                     self.frontier.add_url(scraped_url)
             else:
                 self.logger.info(
-                    f"Skipped {tbd_url}           "
-                    f"using cache {self.config.cache_server}.")
+                    f"Skipped downloading {tbd_url}, robots.txt disallows.")
 
             self.frontier.mark_url_complete(tbd_url)
             time.sleep(self.config.time_delay)
-            no_robot_file_found = False
