197d0115073395f1395bbe1be81af46fe96e9659
Ethan Wong <wongea2@uci.edu>
2023-05-05 10:20:59 -0700

    updated report generation code - follow requested formatting - reconstruct URL from Worker.log

--- report.py
+++ report.py
@@ -1,30 +1,49 @@
-import shelve, sys
+import shelve, sys, re
+from urllib.parse import urlparse
+
+
+def normalize_url(url: str):
+    normalized_url = url
+    if url.endswith("/"):
+        normalized_url = url.rstrip("/")
+    if url.startswith("www."):
+        normalized_url = url.lstrip("www.")
+    return normalized_url.strip()
 
 with shelve.open(sys.argv[-1]) as save:
-    # number of unique pages
+    # number of unique pages - general domains
     general_visited_pages = save['general_visited_pages'] # {domain : # URLs}
-    gen_domains = '\n'.join(general_visited_pages.keys())
-    print("Visited Domains:\n", gen_domains)
-
-    for domain, num_URLs in sorted(general_visited_pages.items(), key=lambda domain: domain[0]):
-        print(f'{domain} : {num_URLs} pages')
-    
     ics_pages = save['ics_visited_pages'] # {ICS domain : {URLs}}
-    print('ICS Domains (ordered):\n')
-    for domain, num_URLs in sorted(ics_pages.items(), key=lambda domain: domain[0]):
-        print(f'{domain} : {num_URLs} pages')
-    
+
+    print('#1 = Unique Pages Found')
     unique_num = sum(general_visited_pages.values()) + sum(ics_pages.values())
     print(f'Unique Pages: {unique_num}', '\n')
-    
-    # longest page
+
+    print('#2 = Longest Page (token word counts, including valid stopwords)')
     print(f"Longest Encountered Page Length: {save['max_words']}", '\n')
-    
+
+    print('#3 = 50 most common non stop words')
     # 50 most common words
-    word_freqs = save['word_frequencies'] # {word: frequency}
+    word_freqs = save['word_frequencies']  # {word: frequency}
     print('Word Frequencies: \n')
-    for word, freq in sorted(word_freqs.items(), key = lambda val : (-val[1], val[0]))[:50]:
-        print(f'{word} = {freq}')
+    counter = 1
+    for word, freq in sorted(word_freqs.items(), key=lambda val: (-val[1], val[0]))[:50]:
+        print(f'#{counter:>4}: {word:>20}, {freq}')
+        counter += 1
+
+    print('#4 = ICS subdomains and unique page count, ordered alphabetically')
+    # reconstruct original URL based on Log
+    with open('Worker.log') as f:
+        for line in f:
+            urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', line)
+            for url in urls:
+                parsed = urlparse(url)
+                if normalize_url(parsed.hostname) in ics_pages:
+                    ics_pages[normalize_url(parsed.hostname)] = url
+
+    counter = 1
+    for url, num_pages in sorted(ics_pages.items(), key=lambda domain: domain[0]):
+        print(f'{counter:>5f} = {url:>100}, {num_pages}')
         
     
     
